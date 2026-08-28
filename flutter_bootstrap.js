aa6428cf6ff2f7d775bb206e6afe4b2833d2cd67
g-scanner <g-scanner@users.noreply.github.com>
2026-08-28 23:09:52 +0000

    deploy: a737bdd5d282d86495550f8525bddd2c424d6eff

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "295105562" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2404201314" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
