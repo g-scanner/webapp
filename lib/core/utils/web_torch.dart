fb3057b75eeb4647c10d3c1fd42b770707fd44ee
devma <manux895@gmail.com>
2026-09-25 19:41:22 +0200

    Rimozione torcia da web siccome non si può usare stabilmente

--- lib/core/utils/web_torch.dart
+++
@@ -1,5 +0,0 @@
-﻿// Copyright (c) 2026 Emanuele Ciotola. All Rights Reserved.
-// PROJECT: G-Scanner — See LICENSE file in root for terms.
-
-export 'web_torch_stub.dart'
-    if (dart.library.js_interop) 'web_torch_web.dart';
--- lib/core/utils/web_torch_stub.dart
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2026 Emanuele Ciotola. All Rights Reserved.
-// PROJECT: G-Scanner — See LICENSE file in root for terms.
-
-/// Stub per piattaforme non-web (Android, iOS, desktop).
-Future<bool> jsHasWebTorch() async => false;
-
-Future<bool> jsToggleWebTorch(bool on) async => false;
